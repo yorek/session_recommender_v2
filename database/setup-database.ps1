03399e37b20a8b14c669027ec580825c7d0273ed
Davide Mauri <damauri@microsoft.com>
2024-03-10 13:34:05 -0700

    deploy database

--- database/setup-database.ps1
+++ database/setup-database.ps1
@@ -6,8 +6,6 @@ chmod a+x ./sqlpackage/sqlpackage
 
 ./sqlpackage/sqlpackage /version
 
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/yorek/session_recommender_v2/main/database/session_recommender_v2.dacpac -OutFile session_recommender_v2.dacpac
+Invoke-WebRequest -Uri https://github.com/yorek/session_recommender_v2/raw/main/database/session_recommender_v2.dacpac -OutFile session_recommender_v2.dacpac
 
-
-SqlPackage /Action:Publish /SourceFile:"C:\AdventureWorksLT.dacpac" \
-    /TargetConnectionString:"Server=tcp:{yourserver}.database.windows.net,1433;Initial Catalog=AdventureWorksLT;Persist Security Info=False;User ID=sqladmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+./sqlpackage/sqlpackage /Action:Publish /SourceFile:"session_recommender_v2.dacpac" /TargetDatabaseName:${DBNAME} /TargetServerName:${DBSERVER} /TargetUser:${SQLADMIN} /TargetPassword:${SQLCMDPASSWORD}
